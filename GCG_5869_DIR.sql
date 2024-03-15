04d067eeed4e28f3c07e07c91a467dc5f6172b59
Service Web <sw@gssmail.com>
2024-03-15 09:00:09 -0500

    Commit changes in C:\Websites\Git\emarlow\5869\GCG_5869_DIR.sql

---
+++ GCG_5869_DIR.sql
@@ -0,0 +1,10 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Mar 18, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5869_DIR" USING 'GCG_5869_DIR.mkd' PAGESIZE=4096 (
+ "Directory" NCHAR(200),
+ "Days" NUMERIC(8,0) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
